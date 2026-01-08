a8b353e6619b9bf8e6f33a0e27d16aa7a434b903
Marsel Khasanov <m.khasanov@uzum.com>
2026-01-08 16:36:17 +0100

    Initial Commit

---
+++ PassCard/PassCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassCardApp.swift
+//  PassCard
+//
+//  Created by needsomevibe on 08/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PassCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
